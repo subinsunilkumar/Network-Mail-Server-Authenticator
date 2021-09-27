37728be9fd7d8f7e9bb8f74c8f174d8eb153ce37
Subin Sunilkumar <48793401+subinsunilkumar@users.noreply.github.com>
2021-09-27 16:38:48 +0200

    Add files via upload
    V1.0 Files added

---
+++ Mail Server/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace Execute_Selected_Tests
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button55 = new System.Windows.Forms.Button();
+            this.button33 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(214)))), ((int)(((byte)(219)))), ((int)(((byte)(233)))));
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.button55);
+            this.panel1.Controls.Add(this.button33);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(500, 59);
+            this.panel1.TabIndex = 0;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Bold);
+            this.label6.Location = new System.Drawing.Point(68, 14);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(131, 30);
+            this.label6.TabIndex = 3;
+            this.label6.Text = "Mail Server";
+            // 
+            // button55
+            // 
+            this.button55.FlatAppearance.BorderSize = 0;
+            this.button55.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button55.Image = global::Execute_Selected_Tests.Properties.Resources.rsz_minimize_minus_remove_sign_icon_1320085940403912850;
+            this.button55.Location = new System.Drawing.Point(426, 0);
+            this.button55.Name = "button55";
+            this.button55.Size = new System.Drawing.Size(38, 48);
+            this.button55.TabIndex = 2;
+            this.button55.UseVisualStyleBackColor = true;
+            // 
+            // button33
+            // 
+            this.button33.FlatAppearance.BorderSize = 0;
+            this.button33.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button33.Image = global::Execute_Selected_Tests.Properties.Resources.rsz_1rsz_close_black_1024x1024;
+            this.button33.Location = new System.Drawing.Point(460, 0);
+            this.button33.Name = "button33";
+            this.button33.Size = new System.Drawing.Size(38, 48);
+            this.button33.TabIndex = 1;
+            this.button33.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Execute_Selected_Tests.Properties.Resources.rsz_1rsz_repo_logo;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 9);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(51, 50);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.White;
+            this.panel2.Controls.Add(this.button1);
+            this.panel2.Location = new System.Drawing.Point(3, 65);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(497, 196);
+            this.panel2.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(169, 92);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(122, 40);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Send mail";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(57)))), ((int)(((byte)(85)))));
+            this.ClientSize = new System.Drawing.Size(501, 265);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Execute Selected Tests";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button33;
+        private System.Windows.Forms.Button button55;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Mail Server/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlTypes;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Runtime.InteropServices;
+using System.Security.AccessControl;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace Execute_Selected_Tests
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                // first we want to setup the mail client
+                var hostName = Dns.GetHostName();
+                // Get the IP
+                var myIP = Dns.GetHostAddresses(hostName)[1].ToString();
+                var condition = myIP.StartsWith("10.3");
+                var address = condition ? "10.3.50.2" : "10.1.1.2";
+                const int port = 587;
+                const string user = "mail_comtestmanager";
+                const string pass = "knPIZ2!Ut%VqTQMZBhGuieO8";
+
+                var client = new SmtpClient
+                {
+                    Host = address,
+                    Port = port,
+                    UseDefaultCredentials = false,
+                    Credentials = new NetworkCredential(user, pass),
+                    EnableSsl = true
+                    
+                };
+
+                var cert = new X509Certificate2();
+                cert.Import(File.ReadAllBytes(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments),
+                    condition ? "letsencrypt-f1wall-dd.ipetronik.com.pem" : "letsencrypt-f1wall.ipetronik.com.pem")));
+                client.ClientCertificates.Add(cert);
+                MailMessage mail = new MailMessage();
+                mail.From = new MailAddress("comtest@ipetronik.com");
+                mail.To.Add("subin.sunilkumar@ipetronik.com");
+                client.Send(mail);
+                MessageBox.Show("mail Send");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+    }
+    
+}
